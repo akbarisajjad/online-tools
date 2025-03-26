7085a76612ff9ca2f63a7ff2ca0fc6dbb62463d6
sajjad akbari <seokarid@gmail.com>
2025-03-26 11:57:53 +0330

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,43 @@
+<footer class="site-footer">
+    <div class="container">
+        <div class="footer-grid">
+            <div class="footer-col">
+                <h3>درباره ابزارک</h3>
+                <p>مجموعه کاملی از ابزارهای آنلاین برای توسعه‌دهندگان و متخصصین فناوری اطلاعات</p>
+            </div>
+            
+            <div class="footer-col">
+                <h3>لینک‌های سریع</h3>
+                <ul>
+                    <li><a href="index.php">خانه</a></li>
+                    <li><a href="#">تماس با ما</a></li>
+                    <li><a href="#">قوانین استفاده</a></li>
+                    <li><a href="#">حریم خصوصی</a></li>
+                </ul>
+            </div>
+            
+            <div class="footer-col">
+                <h3>ابزارهای پرکاربرد</h3>
+                <ul>
+                    <li><a href="tools/data-conversion/json-formatter.php">JSON Formatter</a></li>
+                    <li><a href="#">Base64 Encoder</a></li>
+                    <li><a href="#">Password Generator</a></li>
+                    <li><a href="#">QR Code Generator</a></li>
+                </ul>
+            </div>
+            
+            <div class="footer-col">
+                <h3>ارتباط با ما</h3>
+                <ul class="social-links">
+                    <li><a href="#"><i class="icon-github"></i> GitHub</a></li>
+                    <li><a href="#"><i class="icon-twitter"></i> Twitter</a></li>
+                    <li><a href="#"><i class="icon-telegram"></i> Telegram</a></li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="footer-bottom">
+            <p>© ۲۰۲۳ ابزارک آنلاین. تمام حقوق محفوظ است.</p>
+        </div>
+    </div>
+</footer>
